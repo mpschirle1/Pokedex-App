d885a9eb5f43d686359a3849de02d55989193b43
Matt <mpschirle1@gmail.com>
2022-04-25 23:01:33 -0700

    Created pokemonList array
    Added first 12 pokemon objects to the array.

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+let pokemonList = [
+  {
+    indexNum: 001,
+    name: 'Bulbasaur',
+    type: ['Grass', 'Poison']
+  },
+  {
+    indexNum: 002,
+    name: 'Ivysaur',
+    type: ['Grass', 'Poison']
+  },
+  {
+    indexNum: 003,
+    name: 'Venusaur',
+    type: ['Grass', 'Poison']
+  },
+  {
+    indexNum: 004,
+    name: 'Charmander',
+    type: 'Fire'
+  },
+  {
+    indexNum: 005,
+    name: 'Charmeleon',
+    type: 'Fire'
+  },
+  {
+    indexNum: 006,
+    name: 'Charizard',
+    type: ['Fire', 'Flying']
+  },
+  {
+    indexNum: 007,
+    name: 'Squirtle',
+    type: 'Water'
+  },
+  {
+    indexNum: 008,
+    name: 'Wartortle',
+    type: 'Water'
+  },
+  {
+    indexNum: 009,
+    name: 'Blastoise',
+    type: 'Water'
+  },
+  {
+    indexNum: 010,
+    name: 'Caterpie',
+    type: 'Bug'
+  },
+  {
+    indexNum: 011,
+    name: 'Metapod',
+    type: 'Bug'
+  },
+  {
+    indexNum: 012,
+    name: 'Butterfree',
+    type: ['Bug', 'Flying']
+  },
+];
