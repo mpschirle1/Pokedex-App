4719e54762c1fee9f83331e803846ac979ffd8ae
Matt <mpschirle1@gmail.com>
2022-04-27 15:02:13 -0700

    Added height to pokemonList objects

--- js/scripts.js
+++ js/scripts.js
@@ -2,62 +2,74 @@ let pokemonList = [
   {
     indexNum: '#001',
     name: 'Bulbasaur',
-    type: ['Grass', 'Poison']
+    type: ['Grass', 'Poison'],
+    height: 28
   },
   {
     indexNum: '#002',
     name: 'Ivysaur',
-    type: ['Grass', 'Poison']
+    type: ['Grass', 'Poison'],
+    height: 39
   },
   {
     indexNum: '#003',
     name: 'Venusaur',
-    type: ['Grass', 'Poison']
+    type: ['Grass', 'Poison'],
+    height: 79
   },
   {
     indexNum: '#004',
     name: 'Charmander',
-    type: 'Fire'
+    type: 'Fire',
+    height: 24
   },
   {
     indexNum: '#005',
     name: 'Charmeleon',
-    type: 'Fire'
+    type: 'Fire',
+    height: 43
   },
   {
     indexNum: '#006',
     name: 'Charizard',
-    type: ['Fire', 'Flying']
+    type: ['Fire', 'Flying'],
+    height: 67
   },
   {
     indexNum: '#007',
     name: 'Squirtle',
-    type: 'Water'
+    type: 'Water',
+    height: 20
   },
   {
     indexNum: '#008',
     name: 'Wartortle',
-    type: 'Water'
+    type: 'Water',
+    height: 39
   },
   {
     indexNum: '#009',
     name: 'Blastoise',
-    type: 'Water'
+    type: 'Water',
+    height: 63
   },
   {
     indexNum: '#010',
     name: 'Caterpie',
-    type: 'Bug'
+    type: 'Bug',
+    height: 12
   },
   {
     indexNum: '#011',
     name: 'Metapod',
-    type: 'Bug'
+    type: 'Bug',
+    height: 28
   },
   {
     indexNum: '#012',
     name: 'Butterfree',
-    type: ['Bug', 'Flying']
+    type: ['Bug', 'Flying'],
+    height: 43
   },
 ];
 
