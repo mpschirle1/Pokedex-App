0c80b062d020ab81d10cd3c61a1edeb2b1ebdcc0
Matt <mpschirle1@gmail.com>
2022-05-06 19:52:48 -0700

    Updated forEach function
    Updated forEach function to access pokemonList within the IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -85,7 +85,7 @@ let pokemonRepository = (function () {
 })();
 
 // Lists all Pokémon preceeded by index number, followed by weight.
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write(pokemon.indexNum + " - " + pokemon.name + " (Height: " + pokemon.height + "\")")
   // Adds label "Wow, that's huge!" to Pokémon at least 79 inches tall.
     if (pokemon.height >= 79) {
